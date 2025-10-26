83e6e587c54d4006a0139743af3246f1c27f0966
KhaledAhmed2004 <khaledahmednayeem2004@gmail.com>
2025-10-27 03:36:14 +0600

    udpate

--- src/app/(jobSeeker)/layout.jsx
+++ src/app/(jobSeeker)/layout.jsx
@@ -1,13 +1,14 @@
-
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 
 export default function JobSeekerLayout({ children }) {
   return (
-      <>
-        <Navbar />
+    <>
+      <Navbar />
+      <main className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
         {children}
-        <Footer />
-      </>
+      </main>
+      <Footer />
+    </>
   );
 }
